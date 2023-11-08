a5b975704dd20c1bb2a47faa5ab156d2e4805a07
Ashish Langhe <ashish.langhe@globant.com>
2023-11-08 09:44:00 +0530

    Initial Commit

---
+++ AIChatBot/AIChatBotApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatBotApp.swift
+//  AIChatBot
+//
+//  Created by Ashish Langhe on 08/11/23.
+//
+
+import SwiftUI
+
+@main
+struct AIChatBotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AIChatBot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AIChatBot
+//
+//  Created by Ashish Langhe on 08/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
